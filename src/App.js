4e95e73af3ad9cef9fa5dfcf51cec139b0bd0095
Sopruk <alexandr.programer@gmail.com>
2020-10-14 12:05:19 +0200

    added react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Amazon CLONE</h1>
-    </div>
+    <Router>
+      <div className="app">
+        <h1>Amazon CLONE</h1>
+        <Switch>
+          <Route path="/checkout">
+            <h1>checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>login</h1>
+          </Route>
+          <Route patch="/">
+            <h1>Homme page!</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
