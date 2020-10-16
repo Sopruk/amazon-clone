9fa76f68a7aa7ff08b301a6188f41348457001cd
Sopruk <alexandr.programer@gmail.com>
2020-10-16 15:14:05 +0200

    edited styles from header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import "./Header.css";
 
-function Header() {
+const Header = () => {
   return (
     <nav className="header">
       <Link to="/">
@@ -11,9 +13,43 @@ function Header() {
           src="http://pngimg.com/uploads/amazon/amazon_PNG25.png"
         />
       </Link>
-      <input type="text" className="header__searchInput" />
+      <div className="header__search">
+        <input type="text" className="header__searchInput" />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      <div className="header__nav">
+        <Link to="/login" className="header__link">
+          <div className="header__option">
+            <span className="header__option_lineOne">Hello</span>
+            <span className="header__option_lineTwo">Sign IN</span>
+          </div>
+        </Link>
+
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__option_lineOne">Return</span>
+            <span className="header__option_lineTwo">& Orders</span>
+          </div>
+        </Link>
+
+        <Link to="/" className="header__link">
+          <div className="header__option">
+            <span className="header__option_lineOne">Your</span>
+            <span className="header__option_lineTwo">Prime</span>
+          </div>
+        </Link>
+
+        <Link to="/checkout" className="header__link">
+          <div className="header__optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header__option_lineTwo header__basketCount">
+              0
+            </span>
+          </div>
+        </Link>
+      </div>
     </nav>
   );
-}
+};
 
 export default Header;
