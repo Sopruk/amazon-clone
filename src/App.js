f50e36c9ffb4c42c2afccacce59a3b682c5643e5
Sopruk <alexandr.programer@gmail.com>
2020-11-28 13:03:49 +0100

    Added new Home component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Header from "./components/Header/Header";
-import Home from "./pages/Home";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <div className="app">
         <Switch>
           <Route path="/checkout">
+          <Header />
             <h1>checkout</h1>
           </Route>
           <Route path="/login">
---
+++ src/pages/Home/Home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <img className="home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Events/2020/CyberMonday/Fuji_TallHero_CM_v2_en_US_1x._CB414209152_.png"></img>
+    </div>
+  );
+};
+
+export default Home;
